b3ceab73d20bfe40d9b8a7fe80a230823c3c8f5d
mambo-dev <mambo.michael.22@gmail.com>
2023-07-09 20:04:44 +0300

    change schema to accomodate meals

---
+++ src/app/api/meal-plan/generate-meal-plan/route.ts
@@ -0,0 +1,79 @@
+import { NextResponse } from "next/server";
+import { HandleError } from "../../../../lib/type";
+import { cookies } from "next/headers";
+import verifyAuth from "../../../../lib/auth";
+import { db } from "../../../../lib/prisma";
+
+export async function GET(request: Request): Promise<
+  NextResponse<{
+    data?: boolean;
+    error?: HandleError | HandleError[] | null;
+  }>
+> {
+  try {
+    // get user authentication status
+
+    const cookie = cookies();
+
+    const access_token = cookie.get("access_token");
+
+    if (!cookie) {
+      return NextResponse.json(
+        {
+          error: {
+            message: "could not retrive cookies",
+          },
+        },
+        { status: 400 }
+      );
+    }
+
+    const { user, error } = await verifyAuth(access_token?.value);
+
+    if (error || !user) {
+      return NextResponse.json(
+        {
+          error: {
+            message: error,
+          },
+        },
+        { status: 403 }
+      );
+    }
+
+    const find_user_diet_plan = await db.dietPlan.findUnique({
+      where: {
+        dietplan_user_id: user.user_id,
+      },
+    });
+
+    if (!find_user_diet_plan) {
+      return NextResponse.json(
+        {
+          error: {
+            message: "could not find a diet plan associated with this user",
+          },
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        data: true,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: {
+          message: "something went wrong with the server",
+        },
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
