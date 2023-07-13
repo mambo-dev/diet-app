a5cb456537120ebacff8e4a06db4e63438104e3d
mambo-dev <mambo.michael.22@gmail.com>
2023-07-13 21:15:58 +0300

    frontend sing in and sign up page start

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+type Props = {};
+
+const SignUpPage = (props: Props) => {
+  //two sides the signup inputs and the banner accompanying it
+  //requirements button,inputs,schema validation
+  //toast mechanism
+  return <div>SignUpPage</div>;
+};
+
+export default SignUpPage;
