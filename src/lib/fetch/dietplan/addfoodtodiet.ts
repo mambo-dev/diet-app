44d0b449c7f3bcfac2b22126620c42bded476b65
mambo-dev <mambo.michael.22@gmail.com>
2023-08-06 20:32:08 +0300

    fix: remove commented code

--- src/lib/fetch/dietplan/addfoodtodiet.ts
+++ src/lib/fetch/dietplan/addfoodtodiet.ts
@@ -19,31 +19,9 @@ export default async function add_food_to_diet(
     });
 
     const data = (await res.json()) as ServerResponse<boolean>;
-    console.log(JSON.stringify(body));
     if (data.error && !data.data) {
       console.log(data.error);
-      throw new Error("so wrong");
-      // if (data.error instanceof z.ZodError) {
-      //   const errors = data.error.map((error) => {
-      //     console.log(error.code);
-      //     if (error.code === "invalid_type") {
-      //       return {
-      //         message: "Oops sorry this is not you fault tho",
-      //       };
-      //     }
-      //     return {
-      //       message: error.message,
-      //     };
-      //   });
-
-      //   throw new Error(JSON.stringify(errors));
-      // }
-
-      // if (data.error instanceof Array) {
-      //   throw new Error(JSON.stringify(data.error));
-      // }
-
-      // throw new Error(data.error ?? "something unexpected happened");
+      throw new Error("something went wrong");
     }
     return data.data;
   } catch (error: any) {
